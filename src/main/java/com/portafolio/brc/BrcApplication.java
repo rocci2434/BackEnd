7f08478d5793b0186b48f67dfa9c19baa04f4373
rocci2434 <rociocamilabustamante@gmail.com>
2023-01-28 13:07:34 -0300

    Creacion Back End

---
+++ src/main/java/com/portafolio/brc/BrcApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.brc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrcApplication.class, args);
+	}
+
+}

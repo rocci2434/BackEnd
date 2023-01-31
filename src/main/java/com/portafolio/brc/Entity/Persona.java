fe3fc8ddf46b4235b82e58522d2824792f686f88
rocci2434 <rociocamilabustamante@gmail.com>
2023-01-31 12:31:27 -0300

    BackEnd

---
+++ src/main/java/com/portafolio/brc/Entity/Persona.java
@@ -0,0 +1,32 @@
+package com.portafolio.brc.Entity;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String apellido;
+    
+    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    private String img;
+
+       
+    
+}
---
+++ src/main/java/com/portafolio/brc/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portafolio.brc.Repository;
+
+import com.portafolio.brc.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}

8aadcfe5c4aae5f9e70f43c8f3043b6e3f3b805d
rocci2434 <rociocamilabustamante@gmail.com>
2023-02-03 18:02:36 -0300

    Back End

--- src/main/java/com/portafolio/brc/Security/Enums/RolNombre.java
+++
@@ -1,5 +0,0 @@
-package com.portafolio.brc.Security.Enums;
-
-public enum RolNombre {
-    ROLE_ADMIN, ROLE_USER
-}
---
+++ src/test/java/com/portfolio/brc/BrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.brc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
